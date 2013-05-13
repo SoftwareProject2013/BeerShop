011703c87373cd22302adfa883445a4f44870c08
mczyszkiewicz <mczyszkiewicz@gmail.com>
2013-05-13 11:57:12 +0200

    new branch

--- BeerShop/Controllers/ItemsController.cs
+++ BeerShop/Controllers/ItemsController.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BeerShop.Models;
+using PagedList;
+using BeerShop.Models;
 
 namespace BeerShop.Controllers
 {
@@ -16,11 +18,69 @@ namespace BeerShop.Controllers
         //
         // GET: /Items/
 
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page,string beerCountry,string beerType )
         {
-            return View(db.Items.ToList());
-        }
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "Name desc" : "";
+            ViewBag.PriceSort = sortOrder == "Price" ? "Price desc" : "Price" ;
+            ViewBag.beerCountry = beerCountry;
+            ViewBag.beerType = beerType;
 
+            if (Request.HttpMethod == "GET")
+            {
+                searchString = currentFilter;
+            }
+            else
+            {
+                page = 1;
+            }
+            ViewBag.CurrentFilter = searchString;
+            var items = from i in db.Items select i;
+            if (beerCountry != null)
+            {
+                items = (items.Where(i => i.categories.Contains(db.CategoryItems.FirstOrDefault(c => c.name == beerCountry))));
+            }
+            if (beerType != null)
+            {
+                items = (items.Where(i => i.categories.Contains(db.CategoryItems.FirstOrDefault(c => c.name == beerType))));
+            }
+
+           
+            if (!String.IsNullOrEmpty(searchString))
+            {
+               items = items.Where(s => s.name.ToUpper().Contains(searchString.ToUpper()) );
+            }
+
+            switch (sortOrder)
+            {
+                case  "Name desc":
+                    items = items.OrderByDescending(i => i.name);
+                    break;
+                case "Price":
+                    items= items.OrderByDescending(i => i.Price);
+                    break;
+                case "Price desc" :
+                    items= items.OrderBy(i => i.Price);
+                    break;
+                default :
+                    items = items.OrderBy(i => i.name);
+                    break;
+            }
+
+
+            int PageSize = 3;
+            int pagenumber = (page ?? 1);
+
+            ViewBag.PermissionLevel = Worker.masterPermission;
+
+
+            var categories = from c in db.Categories select c;
+
+            
+            return View(items.ToPagedList(pagenumber,PageSize));
+
+        }
+        
         //
         // GET: /Items/Details/5
 
@@ -39,6 +99,26 @@ namespace BeerShop.Controllers
 
         public ActionResult Create()
         {
+
+           // Where(c => c.category.name.Equals("by Country")).
+            
+            var countryList = db.CategoryItems.Where(c => c.category.name.Equals("by Country"));
+            SelectList SelectCategoryList = new SelectList(countryList, "CategoryItemID", "name");
+            //foreach (CategoryItem c in countryList)
+            //{
+            //    SelectCategoryList.Add(new SelectListItem { Text = c.name, Value = c.CategoryItemID.ToString() });
+            //}
+
+            ViewBag.countriesList = SelectCategoryList;
+
+            var typesList = db.CategoryItems.Where(c => c.category.name.Equals("by Type"));
+            SelectList SelectTypeList = new SelectList(typesList, "CategoryItemID", "name");
+            //foreach (CategoryItem c in typesList)
+            //{
+            //    SelectTypeList.Add(new SelectListItem { Text = c.name, Value = c.CategoryItemID.ToString() });
+            //}
+
+            ViewBag.typesList = SelectTypeList;
             return View();
         }
 
@@ -46,16 +126,20 @@ namespace BeerShop.Controllers
         // POST: /Items/Create
 
         [HttpPost]
-        public ActionResult Create(Item item)
+        public ActionResult Create(ItemCategoryHelper itemHelper)
         {
             if (ModelState.IsValid)
             {
-                db.Items.Add(item);
+                db.Items.Add(itemHelper.item);
+                int countryID = Integer.Parse(itemHelper.SelectedCountry);
+                    itemHelper.SelectedType;
+                db.CategoryItems.FirstOrDefault(c => c.CategoryItemID.ToString().Equals(itemHelper.SelectedCountry)).items.Add(itemHelper.item);
+                db.CategoryItems.FirstOrDefault(c => c.CategoryItemID.ToString().Equals(itemHelper.SelectedType)).items.Add(itemHelper.item);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(item);
+            return View(itemHelper.item);
         }
 
         //
---
+++ BeerShop/Models/ItemCategoryHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+
+namespace BeerShop.Models
+{
+    public class ItemCategoryHelper
+    {
+      public  Item item { set; get; }
+
+      public string SelectedCountry { set; get; }
+      public string SelectedType { set; get; }
+
+    }
+}
